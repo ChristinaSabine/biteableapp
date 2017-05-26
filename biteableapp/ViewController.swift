d3b67d756b2e43b43dfe235f426ef8ff8dacf3ef
Tina Sabine <TinaSabine@Tinas-MacBook-Pro.local>
2017-05-26 12:12:33 +0100

    Initial Commit

---
+++ biteableapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  biteableapp
+//
+//  Created by Tina Sabine on 26/05/2017.
+//  Copyright © 2017 Tina Sabine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
