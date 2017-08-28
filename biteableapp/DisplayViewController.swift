b9ede41372cfc8c78f5bf76163f6f5cf5ace59c0
Tina Sabine <TinaSabine@Tinas-MacBook-Pro.local>
2017-08-28 14:10:09 +0100

    itemlabel

--- biteableapp/DisplayViewController.swift
+++ biteableapp/DisplayViewController.swift
@@ -12,16 +12,14 @@ import UIKit
 class DisplayViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var itemLabel: UILabel!
+
     var item: Item!
     
     override func viewDidLoad() {
         
-        let imageUrl:String = "http://46.101.41.155/img/5000328482835.png"
-        
-        let _:URL = URL(string: imageUrl)!
-        
         print(item)
-        
+            itemLabel.text = "\(item)"
     }
     
     @IBAction func back(_ sender: Any) {
