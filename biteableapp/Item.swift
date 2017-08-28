eea6f784e9a2f7eb13bbe063644327facc4ee139
Tina Sabine <TinaSabine@Tinas-MacBook-Pro.local>
2017-08-28 20:10:58 +0100

    itemdisplay

--- biteableapp/Item.swift
+++ biteableapp/Item.swift
@@ -13,9 +13,15 @@ class Item: CustomStringConvertible {
     let id: Int
     let name: String
     let barcode: String
-    let vegetarian:String
+    let vegetarian: String
+    let vegan: String
+    let glutenfree: String
+    let lactosefree: String
+    let nutfree: String
+    let seafoodfree: String
+    let soyafree: String
     var description: String {
-        return "Product Name: \(name), Barcode:\(barcode), Is it Vegetarian?: \(vegetarian)"
+        return "Product Name - \(name)\n\n Vegetarian? - \(vegetarian)\n Vegan? - \(vegan)\n Gluten Free? - \(glutenfree)\n Lactose Free? - \(lactosefree)\n Nut Free? - \(nutfree)\n Seafood Free? - \(seafoodfree)\n Soya Free? - \(soyafree)\n\n Barcode - \(barcode)"
     }
     
     var imageURL: String {
@@ -27,5 +33,11 @@ class Item: CustomStringConvertible {
         self.name = json["name"].stringValue
         self.barcode = json["barcode"].stringValue
         self.vegetarian = json["vegetarian"].stringValue
+        self.vegan = json["vegan"].stringValue
+        self.glutenfree = json["glutenfree"].stringValue
+        self.lactosefree = json["lactosefree"].stringValue
+        self.nutfree = json["nutfree"].stringValue
+        self.seafoodfree = json["seafoodfree"].stringValue
+        self.soyafree = json["soyafree"].stringValue
     }
 }
